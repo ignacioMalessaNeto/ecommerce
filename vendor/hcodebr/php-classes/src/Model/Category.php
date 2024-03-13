2a0ccdf75470d5ae3c82bb1074501ade2adf5500
Ignacio Neto <ignacio.neto@1r.local>
2024-03-13 11:54:22 -0300

    Paginação de categorias

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -77,42 +77,74 @@ class Category extends Model
                     FROM tb_products a
                     INNER JOIN tb_productscategories b ON a.idproduct = b.idproduct
                     WHERE b.idcategory = :idcategory
-                );",[
-                    ':idcategory'=>$this->getidcategory()
+                );",
+                [
+                    ':idcategory' => $this->getidcategory()
                 ]
             );
         } else {
-             return $sql->select(
+            return $sql->select(
                 "SELECT * FROM tb_products WHERE idproduct NOT IN(
                     SELECT a.idproduct
                     FROM tb_products a
                     INNER JOIN tb_productscategories b ON a.idproduct = b.idproduct
                     WHERE b.idcategory = :idcategory
-                );",[
-                    ':idcategory'=>$this->getidcategory()
+                );",
+                [
+                    ':idcategory' => $this->getidcategory()
                 ]
             );
         }
     }
 
-    public function addProduct(Product $product){
+    public function getProductsPage($page = 1, $itemsPerPage = 3)
+    {
+        $start = ($page - 1) * $itemsPerPage;
+
+        $sql = new Sql();
+
+        $resultProducts = $sql->select("SELECT SQL_CALC_FOUND_ROWS *
+        FROM tb_products a 
+        INNER JOIN tb_productscategories b ON a.idproduct = b.idproduct
+        INNER JOIN tb_categories c ON c.idcategory = b.idcategory
+        WHERE c.idcategory = :idcategory
+        LIMIT $start, $itemsPerPage;
+        ", [
+            ':idcategory'=>$this->getidcategory()
+        ]);
+
+        $totalResults = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+
+        return [
+            'data'=>Product::checkList($resultProducts),
+            'total'=>(int)$totalResults[0]["nrtotal"],
+            'pages'=>ceil($totalResults[0]["nrtotal"] / $itemsPerPage)
+        ];
+
+
+
+
+    }
+
+    public function addProduct(Product $product)
+    {
 
         $sql = new Sql();
 
         $sql->query("INSERT INTO tb_productscategories (idcategory, idproduct) VALUES (:idcategory, :idproduct)", [
-            ':idcategory'=>$this->getidcategory(),
-            ':idproduct'=>$product->getidproduct()
+            ':idcategory' => $this->getidcategory(),
+            ':idproduct' => $product->getidproduct()
         ]);
     }
 
-    public function removeProduct(Product $product){
+    public function removeProduct(Product $product)
+    {
 
         $sql = new Sql();
 
         $sql->query("DELETE FROM tb_productscategories WHERE idcategory = :idcategory AND idproduct = :idproduct", [
-            ':idcategory'=>$this->getidcategory(),
-            ':idproduct'=>$product->getidproduct()
+            ':idcategory' => $this->getidcategory(),
+            ':idproduct' => $product->getidproduct()
         ]);
     }
-
 }
