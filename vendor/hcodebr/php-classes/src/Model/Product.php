8dd500c2fdcf774bb6b8875214124fc5cb014a12
Ignacio Malessa Neto <igmalessa@gmail.com>
2024-04-10 15:38:10 -0300

    Admin - Produto - Busca e páginação

--- vendor/hcodebr/php-classes/src/Model/Product.php
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -153,4 +153,54 @@ class Product extends Model
             ':idproduct'=>$this->getidproduct()
         ]);
     }
+
+    public static function getPage($page = 1, $itemsPerPage = 10)
+    {
+        $start = ($page - 1) * $itemsPerPage;
+
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT SQL_CALC_FOUND_ROWS *
+        FROM tb_products 
+        ORDER BY desproduct
+        LIMIT $start, $itemsPerPage;
+        ");
+
+        $totalResults = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+
+        return [
+            'data'=>$results,
+            'total'=>(int)$totalResults[0]["nrtotal"],
+            'pages'=>ceil($totalResults[0]["nrtotal"] / $itemsPerPage)
+        ];
+
+    }
+
+    public static function getPageSearch($search,$page = 1, $itemsPerPage = 10)
+    {
+        $start = ($page - 1) * $itemsPerPage;
+
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT SQL_CALC_FOUND_ROWS *
+        FROM tb_products 
+        WHERE desproduct LIKE :search
+        ORDER BY desproduct
+        LIMIT $start, $itemsPerPage;
+        ", [
+            ':search'=>'%' .$search. '%'
+        ]);
+
+        $totalResults = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+
+        return [
+            'data'=>$results,
+            'total'=>(int)$totalResults[0]["nrtotal"],
+            'pages'=>ceil($totalResults[0]["nrtotal"] / $itemsPerPage)
+        ];
+
+    }
+
+
+
 }
--- views-cache/categories-menu.c5b813be90a10a19f97859c318d75589.rtpl.php
+++ views-cache/categories-menu.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -1,1 +1,1 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/category/6">Android</a></li><li><a href="/category/2">Apple</a></li><li><a href="/category/4">Motorola</a></li><li><a href="/category/5">Samsung</a></li>
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/category/6">Android</a></li><li><a href="/category/2">Apple</a></li><li><a href="/category/7">Google</a></li><li><a href="/category/4">Motorola</a></li><li><a href="/category/5">Samsung</a></li>
