5e0419a16f3c736033b8d674128e8c705a6088ec
Ignacio Malessa Neto <igmalessa@gmail.com>
2024-04-10 15:28:50 -0300

    Admin - Categorias - Busca Categorias

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -144,4 +144,53 @@ class Category extends Model
             ':idproduct' => $product->getidproduct()
         ]);
     }
+
+    public static function getPage($page = 1, $itemsPerPage = 10)
+    {
+        $start = ($page - 1) * $itemsPerPage;
+
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT SQL_CALC_FOUND_ROWS *
+        FROM tb_categories 
+        ORDER BY descategory
+        LIMIT $start, $itemsPerPage;
+        ");
+
+        $totalResults = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+
+        return [
+            'data'=>$results,
+            'total'=>(int)$totalResults[0]["nrtotal"],
+            'pages'=>ceil($totalResults[0]["nrtotal"] / $itemsPerPage)
+        ];
+
+    }
+
+    public static function getPageSearch($search,$page = 1, $itemsPerPage = 10)
+    {
+        $start = ($page - 1) * $itemsPerPage;
+
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT SQL_CALC_FOUND_ROWS *
+        FROM tb_categories 
+        WHERE descategory LIKE :search
+        ORDER BY descategory
+        LIMIT $start, $itemsPerPage;
+        ", [
+            ':search'=>'%' .$search. '%'
+        ]);
+
+        $totalResults = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+
+        return [
+            'data'=>$results,
+            'total'=>(int)$totalResults[0]["nrtotal"],
+            'pages'=>ceil($totalResults[0]["nrtotal"] / $itemsPerPage)
+        ];
+
+    }
+
+
 }
